bbdb88cc107107c87973d7c54d21c9ba665115ba
jacobshan <jacobshan429@gmail.com>
2016-10-07 14:41:52 +0800

    generated migration to add usr_id column to posts table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(params_post)
+    if @post.save
+      flash[:notice] = "New Post Saved!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Failed to save!"
+      render "new"
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates_presence_of :title
+  belongs_to :user
 end
