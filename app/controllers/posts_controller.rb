a379255bcd96696ec8624df7fc50adbef565261c
jacobshan <jacobshan429@gmail.com>
2016-10-07 15:25:10 +0800

    added edit update delete action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(params_post)
+      flash[:notice] = "Topic Updated!"
+      redirect_to :action => :show
+    else
+      flash[:alert] = "Fail to update!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to :action => :index
+  end
+
   private
   def params_post
     params.require(:post).permit(:title, :content, :category_id)
